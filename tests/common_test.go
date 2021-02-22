dba7670c2e1cf53282d5fa23780396d5c8b47c09
anilkusc <anilkuscu95@gmail.com>
2021-02-22 13:08:09 +0300

    changed unsupported queries

--- tests/common_test.go
+++ tests/common_test.go
@@ -15,9 +15,10 @@ func CreateDatabase(t *testing.T, functionName string) *sql.DB {
 		`CREATE TABLE Users (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Name TEXT NOT NULL UNIQUE);`,
 		`INSERT INTO Users (Name) VALUES ('anonymous');`,
 		`INSERT INTO Users (Name) VALUES ('testuser');`,
-		`CREATE TABLE Sessions (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Date TEXT NOT NULL,End INTEGER NOT NULL DEFAULT 0,Winner INTEGER NOY NULL DEFAULT 0);`,
-		`INSERT INTO Sessions (Date,Winner) VALUES ('Now');`,
-		`CREATE TABLE Moves (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,SessionId INTEGER NOT NULL,Positive INTEGER DEFAULT -1,Negative INTEGER DEFAULT -1,Turn INTEGER NOT NULL,Player1 TEXT,Player2 TEXT,Player1Number INTEGER,Player2Number INTEGER,Predictor INTEGER,Prediction INTEGER,Action TEXT,FOREIGN KEY (SessionId) REFERENCES Sessions (Id) ON DELETE CASCADE);`,
+		`CREATE TABLE Sessions (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Date TEXT NOT NULL,End INTEGER NOT NULL DEFAULT 0,Winner INTEGER NOT NULL DEFAULT 0);`,
+		`INSERT INTO Sessions (Date,Winner) VALUES ('Now',0);`,
+		//It is not support foreign key
+		`CREATE TABLE Moves (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,SessionId INTEGER NOT NULL,Positive INTEGER ,Negative INTEGER ,Turn INTEGER NOT NULL,Player1 TEXT,Player2 TEXT,Player1Number INTEGER,Player2Number INTEGER,Predictor INTEGER,Prediction INTEGER,Action TEXT);`,
 		`INSERT INTO Moves (SessionId,Turn,Action) VALUES (1,0,'Created');`,
 	}
 	//open pseudo database for function
@@ -29,7 +30,7 @@ func CreateDatabase(t *testing.T, functionName string) *sql.DB {
 	for _, b := range batches {
 		_, err = db.Exec(b)
 		if err != nil {
-			t.Fatalf("Error exec query in mock query: %s\n", err)
+			t.Fatalf("Error exec query in query: %s\n Error:%s", b, err)
 		}
 	}
 	return db
