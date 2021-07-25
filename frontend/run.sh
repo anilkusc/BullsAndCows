32b3cbe33fed4d48a0a47e0d1d471461cf516b6a
anilkusc <anilkuscu95@gmail.com>
2021-07-25 16:48:38 +0300

    deleted unnecessary files

--- frontend/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
-GOOS=js GOARCH=wasm go build -o main.wasm main.go 
-GOOS=js GOARCH=wasm go build -o game.wasm game.go
-python3 -m http.server
